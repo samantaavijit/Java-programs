a91dd238b749f0717e4500f0b2589921206ce4bb
Avijit Samanta <samantaavijit.2000@gmail.com>
2020-08-26 09:42:05 +0530

    "Update LargestAndSmallestFrom3 "

--- Collage/LargestAndSmallestFrom3.java
+++ Collage/LargestAndSmallestFrom3.java
@@ -1,7 +1,16 @@
 package com.avijitsamanta.Collage;
+
 import java.util.Scanner;
 
 public class LargestAndSmallestFrom3 {
+    private static int max(int a, int b) {
+        return a > b ? a : b;
+    }
+
+    private static int min(int a, int b) {
+        return a < b ? a : b;
+    }
+
     public static void main(String[] args) {
         System.out.print("Enter three numbers ");
         Scanner sc = new Scanner(System.in);
@@ -9,31 +18,33 @@ public class LargestAndSmallestFrom3 {
         int b = sc.nextInt();
         int c = sc.nextInt();
 
-        if (a > b) {
-            if (a > c) {
-                System.out.println("largest is " + a);
-                if (b > c)
-                    System.out.println("Smallest is " + c);
-                else
-                    System.out.println("Smallest is " + b);
-            } else {
-                System.out.println("largest is " + c);
-                System.out.println("Smallest is " + b);
-            }
-        }
-        else if (b > c) {
-            System.out.println("largest is " + b);
-            if (a > c)
-                System.out.println("Smallest is " + c);
-            else
-                System.out.println("Smallest id " + a);
-        }
-        else {
-            System.out.println("largest is "+c);
-            if (b>a)
-                System.out.println("Smallest is "+a);
-            else
-                System.out.println("Smallest is "+b);
-        }
+//        if (a > b) {
+//            if (a > c) {
+//                System.out.println("largest is " + a);
+//                if (b > c)
+//                    System.out.println("Smallest is " + c);
+//                else
+//                    System.out.println("Smallest is " + b);
+//            } else {
+//                System.out.println("largest is " + c);
+//                System.out.println("Smallest is " + b);
+//            }
+//        } else if (b > c) {
+//            System.out.println("largest is " + b);
+//            if (a > c)
+//                System.out.println("Smallest is " + c);
+//            else
+//                System.out.println("Smallest id " + a);
+//        } else {
+//            System.out.println("largest is " + c);
+//            if (b > a)
+//                System.out.println("Smallest is " + a);
+//            else
+//                System.out.println("Smallest is " + b);
+//        }
+
+        System.out.println("largest is " + max(max(a, b), c));
+        System.out.println("Smallest is " + min(min(a, b), c));
+
     }
 }
